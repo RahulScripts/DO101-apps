4630c238a1003b1e535341e8373c87d6415f958e
RahulScripts <hallirahul777@gmail.com>
2024-12-10 21:52:14 +0530

    added new route

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'version 2 now available on /new!' + '\n';
+    response = 'version 2 and 3 now available on /new! and 3' + '\n';
 
     //send the response to the client
     res.send(response);
@@ -15,6 +15,11 @@ app.get('/', function (req, res) {
 app.get('/new', function (req, res) {
     response = 'This is version 2! Welcome'+'\n';
     res.send(response);
+});
+
+app.get('/3', function (req, res) {
+  response = 'This is version 3! Welcome'+'\n';
+  res.send(response);
 });
 
 app.listen(8080, function () {
